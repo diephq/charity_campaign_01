5e799356d5c12fc9d1fae608e62c756269d1e3dd
diephq <huynh.quang.diep@framgia.com>
2016-11-30 08:49:50 +0700

    Init master page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ trans('message.project') }}</title>
+
+    @section('css')
+        {{ Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') }}
+        {{ Html::style('assets/css/common.css') }}
+    @show
+
+</head>
+<body>
+    <div id="app">
+
+        @include('layouts.header')
+
+        @yield('content')
+
+        @include('layouts.footer')
+
+    </div>
+
+    @section('js')
+        {{ Html::script('bower_components/jquery/dist/jquery.min.js') }}
+        {{ Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') }}
+    @show
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted"></p>
+        <div class="col-md-12 text-center">
+            <p>{{ trans('message.footer') }}</p>
+        </div>
+    </div>
+</footer>
