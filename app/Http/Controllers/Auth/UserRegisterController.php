cbdeda78268f74470a0f62456f82c6e4fbf8e38f
diephq <huynh.quang.diep@framgia.com>
2016-12-01 08:45:52 +0700

    Fix user register

---
+++ app/Http/Controllers/Auth/UserRegisterController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Repositories\User\UserRepositoryInterface;
+
+class UserRegisterController extends Controller
+{
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    protected $user;
+    protected $userRepository;
+
+    /**
+     * UserRegisterController constructor.
+     * @param User $user
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(User $user, UserRepositoryInterface $userRepository)
+    {
+        $this->middleware('guest');
+        $this->user = $user;
+        $this->userRepository = $userRepository;
+    }
+
+    public function getRegister()
+    {
+        return view('auth.register');
+    }
+
+    public function postRegister(Request $request)
+    {
+        $this->validate($request, $this->user->rules);
+
+        $user = $this->userRepository->register($request->all());
+
+        if (empty($user)) {
+            return redirect('/register')->withErrors(['error'=> trans('user.register.error')]);
+        }
+
+        return redirect('/login');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,16 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('register',[
+    'as' => 'register' ,
+    'uses' => 'Auth\UserRegisterController@getRegister',
+]);
+
+Route::post('register',[
+    'as' => 'register' ,
+    'uses' => 'Auth\UserRegisterController@postRegister',
+]);
+
 Route::get('link/verification/{id}/{tokenRegister?}',[
     'as' => 'verification' ,
     'uses' => 'Auth\VerifyController@index',
