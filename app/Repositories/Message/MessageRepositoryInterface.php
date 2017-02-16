3b1bf1feb68aae6e5f6538c3725bd9e0ce955b37
at-diephq <it.huynhdiep@gmail.com>
2017-02-16 11:29:30 +0700

    migrate + init model + repo chat

---
+++ app/Repositories/Message/MessageRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Message;
+
+interface MessageRepositoryInterface
+{
+
+}
---
+++ database/migrations/2017_02_15_022250_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('message');
+            $table->integer('user_id');
+            $table->integer('group_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
